1eb6d37066f6d208e8d54133161b578d2307d4aa
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-10-16 11:46:11 -0600

    Starting

---
+++ Frontend/naive-bayes-c/src/ClassifierApp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ClassifierApp = () => {
+  return (
+    <div>ClassifierApp</div>
+  )
+}
---
+++ Frontend/naive-bayes-c/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ClassifierApp } from './ClassifierApp';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ClassifierApp />
+ 
+);
+
+
