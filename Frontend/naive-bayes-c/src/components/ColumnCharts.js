065d3b66bb95866a3a88656a963b1243e976369a
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-10-23 17:13:44 -0600

    ColumnChart0

--- Frontend/naive-bayes-c/src/components/ColumnCharts.js
+++ Frontend/naive-bayes-c/src/components/ColumnCharts.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import Chart from 'chart.js/auto';
 
 export const ColumnCharts = () => {
+  const data = {
+    labels: ["Uno", "Dos", "Tres"],
+    backgroundColor:'#f38b4a',
+    borderColor : "#fffff",
+    datasets: [
+      {
+        label: 'Links Totales', 
+        backgroundColor: [
+          "#f38b4a"
+        ],
+        borderColor : "black",
+        borderWidth : "3",
+        Color:'blue',
+        borderwidth: 1,
+        hoverBackgroundColor: [
+          "#56d798",
+          "#ff8397",
+          "#6970d5" 
+        ],
+        hoverBorderColor : "#000",
+        data: [100, 300, 200],
+      },
+    ]
+  };
+  const opciones = {
+    maintainAspectRatio: false,
+    responsive: true,
+  };
   return (
-    <div>ColumnCharts</div>
-  )
-}
+      <Bar data={data} options={opciones} />
+  );
+};
--- Frontend/naive-bayes-c/src/pages/ClassifierPage.js
+++ Frontend/naive-bayes-c/src/pages/ClassifierPage.js
@@ -4,16 +4,19 @@ import "../css/Classifier.css";
 
 export const ClassifierPage = () => {
   return (
-    // <div className="flex-container">
-    //   <div>
-    //     <NavLink to="/LinkClassifier">
-    //       <button className="btn-2">Link Classifier</button>
-    //     </NavLink>
-    //   </div>
-    // </div>
-    <ColumnCharts/>
-
-
-
+    <div className="flex-container-2">
+      <div className="flex-container">
+        <div>
+          <NavLink to="/LinkClassifier">
+            <button className="btn-2">Link Classifier</button>
+          </NavLink>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="colum-chart">
+          <ColumnCharts />
+        </div>
+      </div>
+    </div>
   );
 };
