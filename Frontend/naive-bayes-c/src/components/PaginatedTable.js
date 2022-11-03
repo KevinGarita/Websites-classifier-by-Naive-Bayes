1b55e88246c37d91975688dee21da30d6b8fa931
CristopherSC <61480499+CristopherSotoC@users.noreply.github.com>
2022-11-03 09:48:14 -0600

    a

--- Frontend/naive-bayes-c/src/components/PaginatedTable.js
+++ Frontend/naive-bayes-c/src/components/PaginatedTable.js
@@ -2,14 +2,30 @@
 import { Column, TableWithBrowserPagination } from "react-rainbow-components";
 
 export const PaginatedTable = ({dataTable, show}) => {
-  return (
-    <TableWithBrowserPagination
-      keyField={show}
-      data={dataTable[0]}
-      variant="listview"
-      pageSize={3}
-    >
-      <Column header={dataTable[1]} field={show} />
-    </TableWithBrowserPagination>
-  );
+
+  if (show==="word") {
+    return (
+      <TableWithBrowserPagination
+        keyField={show}
+        data={dataTable[0]}
+        variant="listview"
+        pageSize={3}
+      >
+        <Column header={dataTable[1]} field={show} />
+        <Column header={"Number"} field={"count"} />
+      </TableWithBrowserPagination>
+    );
+  } else {
+    return (
+      <TableWithBrowserPagination
+        keyField={show}
+        data={dataTable[0]}
+        variant="listview"
+        pageSize={3}
+      >
+        <Column header={dataTable[1]} field={show} />
+      </TableWithBrowserPagination>
+    );
+  }
+  
 };
